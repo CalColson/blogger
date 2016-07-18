3f1cdfd7e8221266f4e5f5751148949a6450f399
Cal Colson <cpcolson@yahoo.com>
2016-07-18 18:54:20 -0500

    Second Commit for Blogger

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -6,4 +8,15 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.create(article_params)
+
+		redirect_to @article
+	end
+
 end

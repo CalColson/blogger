40df517120797583653b7dd9d5ce2cae9d1eb9c3
Cal Colson <cpcolson@yahoo.com>
2016-07-18 19:40:16 -0500

    third commit

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -13,10 +13,31 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.create(article_params)
 
+		flash.notice = "Article '#{@article.title}' was created!"
 		redirect_to @article
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' was updated!"
+		redirect_to @article
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' was deleted!"
+		redirect_to articles_path
 	end
 
 end
